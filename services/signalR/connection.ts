2aaa0528ade094d9901835a6d567bf2e91f2df2f
Abdus samad <abdussamadkotha@gmail.com>
2026-01-14 12:20:49 +0500

    api endpoint updated

--- services/signalR/connection.ts
+++ services/signalR/connection.ts
@@ -1,6 +1,6 @@
 import * as signalR from '@microsoft/signalr';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://ordering.eatx.pk:8090';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://ordering.eatx.pk';
 
 let connection: signalR.HubConnection | null = null;
 
